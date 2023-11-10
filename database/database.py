eea0658ec24de0bdca99ed06998839d43b64549a
Huryel Souto Costa <huryel.souto@gmail.com>
2023-11-10 18:26:54 -0300

    adding levelDB and Pysync to the project

---
+++ database/database.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+from leveldb import Database
+
+import sys
+import socket
+import threading
+import json
+            
+def funcoesBanco(replica, conn, addr):
+    while True:
+        data = conn.recv(2048)
+        msg = data.decode()
+        print(msg)
+        if msg:
+            responseMsg = json.loads(msg)
+            functionName = responseMsg['function']
+            key = responseMsg['key']
+            value = responseMsg['value']
+            
+        if functionName == 'insert':           
+            replica.insertData(key, value)            
+            resp = json.dumps({'msg': "Insert realizado com sucesso."})
+
+        if functionName == 'read':
+            response = replica.getData(key)
+
+            print(key)
+            print(response)
+            
+            if response != '':
+                response = json.loads(response)    
+            
+            resp = json.dumps({'data': response})
+
+        if functionName == 'delete':
+            replica.deleteData(key)
+            resp = json.dumps({'msg': "Delete realizado com sucesso."})
+
+
+        conn.send(resp.encode())
+
+def run():
+    if len(sys.argv) < 2:
+        sys.exit(-1)
+
+    arg = int(sys.argv[1])
+
+    if arg not in [1, 2, 3]:
+        print("Escolha: 1, 2 ou 3")
+        sys.exit(-1)
+    
+    # Partição 1
+    if arg == 1:
+        portaSocket = 10001
+        replica = Database(portaSocket, 'particao00', 'localhost:20001',['localhost:20002', 'localhost:20003'])
+    if arg == 2:
+        portaSocket = 10002
+        replica = Database(portaSocket, 'particao00', 'localhost:20002',['localhost:20001', 'localhost:20003'])
+    if arg == 3:
+        portaSocket = 10003
+        replica = Database(portaSocket, 'particao00', 'localhost:20003',['localhost:20001', 'localhost:20002'])
+    
+    sock = socket.socket()
+    host = socket.gethostname()
+    sock.bind((host, portaSocket))
+    sock.listen(15)
+    while True:
+        conn, addr = sock.accept()
+        threading.Thread(target=funcoesBanco, args=(replica, conn, addr)).start()
+
+
+if __name__ == '__main__':
+    run()
--- scripts/server.py
+++ scripts/server.py
@@ -9,64 +9,87 @@ from cachetools import LRUCache
 from concurrent import futures
 
 
-# mqtt needed imports
+# # mqtt needed imports
+import json
 import random
-import logging
+import socket
 
-from paho.mqtt import client as mqtt_client
+# import logging
+
+# from paho.mqtt import client as mqtt_client
 
 
-# Create an MQTT Connection
+# # Create an MQTT Connection
 
-broker = 'broker.emqx.io'
-port = 1883
-topic = 'python/key-vserions'
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
+# broker = 'broker.emqx.io'
+# port = 1883
+# topic = 'python/key-vserions'
+# client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
-cache_dictionary = LRUCache(maxsize=100)
 
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print('Connected to MQTT Broker!')
-        else:
-            print('Failed to connect, return code %d\n', rc)
-    # Set Connecting Client ID
-    client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+# def connect_mqtt():
+#     def on_connect(client, userdata, flags, rc):
+#         if rc == 0:
+#             print('Connected to MQTT Broker!')
+#         else:
+#             print('Failed to connect, return code %d\n', rc)
+#     # Set Connecting Client ID
+#     client = mqtt_client.Client(client_id)
+#     # client.username_pw_set(username, password)
+#     client.on_connect = on_connect
+#     client.connect(broker, port)
+#     return client
 
-def publish(client, msg):
-    result = client.publish(topic, msg)
-    status = result[0]
+# def publish(client, msg):
+#     result = client.publish(topic, msg)
+#     status = result[0]
 
-    if status == 0:
-        print(f'Send `{msg}` to topic `{topic}`')
-    else:
-        print(f'Failed to send message to topic {topic}')
+#     if status == 0:
+#         print(f'Send `{msg}` to topic `{topic}`')
+#     else:
+#         print(f'Failed to send message to topic {topic}')
 
-def subscribe(client: mqtt_client):    
-    def on_message(client, userdata, msg):
-        print(f'Received `{msg.payload.decode()}` from `{msg.topic}` topic')
+# def subscribe(client: mqtt_client):    
+#     def on_message(client, userdata, msg):
+#         print(f'Received `{msg.payload.decode()}` from `{msg.topic}` topic')
         
-        mensagem = msg.payload.decode().split('-')
-        key = mensagem[1]
+#         mensagem = msg.payload.decode().split('-')
+#         key = mensagem[1]
         
-        if mensagem[0] == 'UPDATE_KEY':
-            value = mensagem[3]
-            version = int(mensagem[6]) if str(mensagem[6]) != 'None' else None
-            valuesList = valuesList = list(cache_dictionary[key]) if key in cache_dictionary else []
-            valuesList.append((value, version)) 
-            cache_dictionary[key] = valuesList
-        if mensagem[0] == 'DELETE_KEY':
-            del cache_dictionary[key]
+#         if mensagem[0] == 'UPDATE_KEY':
+#             value = mensagem[3]
+#             version = int(mensagem[6]) if str(mensagem[6]) != 'None' else None
+#             valuesList = valuesList = list(cache_dictionary[key]) if key in cache_dictionary else []
+#             valuesList.append((value, version)) 
+#             cache_dictionary[key] = valuesList
+#         if mensagem[0] == 'DELETE_KEY':
+#             del cache_dictionary[key]
 
-        print('New cache')
-        print(cache_dictionary)
-    client.subscribe(topic)
-    client.on_message = on_message
+#         print('New cache')
+#         print(cache_dictionary)
+#     client.subscribe(topic)
+#     client.on_message = on_message
+
+global socketDB
+
+def setReplica():
+    numSocket = random.randint(10001,10003)
+
+    try:
+        hostName = socket.gethostname()
+
+        socketDB = socket.socket()
+        socketDB.settimeout(1)
+
+        socketDB.connect((hostName, numSocket))
+
+        print(f'Server started, connected to DB server on port {numSocket}')
+
+    except:
+        print('Erro na criacao das particoes')
+        sys.exit()
+        
+    return socketDB
 
 
 def timestamp_in_miliseconds():
@@ -81,76 +104,122 @@ class KeyValueStore(project_pb2_grpc.KeyValueStoreServicer):
     #     # Inicialize o cache de tabela hash (substitua o tamanho conforme necessário)
     #     self.cache = LRUCache(maxsize=100)
 
+    def __init__(self) -> None:
+        self.socket = setReplica()
+
     def Put(self, request, context):
         chave = request.key
         valor = request.val
         print(request)
 
-        if chave in cache_dictionary:
-            print(f'{chave} is in cache')
+        socket = self.socket
+
+        msg = json.dumps({'function':'read', 'key':chave, 'value':None})
+        try:
+            socket.send(msg.encode())
+            resposta = socket.recv(2048)
+            resposta = json.loads(resposta.decode())
+        except:
+            print(f'Erro ao consultar chave')
+            return None
             
-            valores = cache_dictionary[chave]
+        if resposta['data'] != '':
+            valores = resposta['data']
             versao_request = timestamp_in_miliseconds()
-            valor_request = valores[-1][0]
-
-
-            resposta = project_pb2.PutReply(
-                key=chave, old_val=valor_request, old_ver= valores[-1][1], ver=versao_request
+            
+            msg = json.dumps({'function':'delete', 'key':chave, 'value':None})
+            socket.send(msg.encode())
+            resposta = socket.recv(2048)
+            
+            msg = json.dumps({'function':'insert', 'key':chave, 'value': valores + [(valor, versao_request)]})
+            socket.send(msg.encode())
+            resposta = socket.recv(2048)
+            
+            return project_pb2.PutReply(
+                key=chave, old_val=valores[-1][0], old_ver= valores[-1][1], ver=versao_request
             )
 
-            print(cache_dictionary)
         else:
-            print(f'{chave} is not in cache')
             versao_request = timestamp_in_miliseconds()
-            valores = []
-            resposta = project_pb2.PutReply(
+           
+            msg = json.dumps({'function':'insert', 'key':chave, 'value':[(valor, versao_request)]})
+            socket.send(msg.encode())
+            resposta = socket.recv(2048)
+
+            return project_pb2.PutReply(
                 key=chave, old_val='', old_ver=0, ver=versao_request
             )
-            print(cache_dictionary)
-        publish(client, f'UPDATE_KEY-{chave}-TO-{valor}-VALUE-IN-{versao_request}-VERSION') 
-        return resposta
+        
     
     def Get(self, request, context):
         chave = request.key
         print(f'Get {chave} key')
 
-        if chave in cache_dictionary:
-            valores = cache_dictionary[chave]
+        socket = None
+        socket = self.socket
+        
+        msg = json.dumps({'function':'read', 'key':chave, 'value':None})
+        try:
+            socket.send(msg.encode())
+            resposta = socket.recv(2048)
+            resposta = json.loads(resposta.decode())
+        except:
+            print(f'Erro ao consultar chave')
+            return None
+        
+        print(resposta)
+        if resposta['data'] == '':
+            print(f'Chave \'{chave}\' não existe')
+        else:
+            valores = resposta['data']
             if(request.ver): 
-                versao = request.ver
+                versao = int(request.ver)
                 for valor, valor_versao in reversed(valores):
-                    if valor_versao <= versao:
+                    if int(valor_versao) <= versao:
                         valor_final = valor
                         versao = valor_versao
                         break
             else:
                 versao = valores[-1][1]
                 valor_final = valores[-1][0]
-            
-            resposta = project_pb2.KeyValueVersionReply(
+            return project_pb2.KeyValueVersionReply(
                 key=chave, val=valor_final, ver=versao
-            )
-            
-            return resposta
-                
-        else: print('chave não existe')
+            ) 
 
     def Del(self, request, context):
         chave = request.key
         print(f'Get {chave} key')
 
-        if chave in cache_dictionary:
-            valores = cache_dictionary[chave]
+        socket = None
+        socket = self.socket
+        
+        msg = json.dumps({'function':'read', 'key':chave, 'value':None})
+        try:
+            socket.send(msg.encode())
+            resposta = socket.recv(2048)
+            resposta = json.loads(resposta.decode())
+        except:
+            print(f'Erro ao consultar chave')
+            return None
+            
+        if resposta['data'] == '':
+            print(f'Chave \'{chave}\' não existe')
+            return project_pb2.KeyValueVersionReply(
+                key='', val='', ver=0
+            )
+        else:
+            print('AAAAAAAAA')
+            valores = resposta['data']
             versao = valores[-1][1]
             valor = valores[-1][0]
-            resposta = project_pb2.KeyValueVersionReply(
+
+            msg = json.dumps({'function':'delete', 'key':chave, 'value':None})
+            socket.send(msg.encode())
+            resposta = socket.recv(2048)
+
+            return project_pb2.KeyValueVersionReply(
                 key=chave, val=valor, ver=versao
             )
-
-            publish(client, f'DELETE_KEY-{chave}') 
-
-            return resposta
-        else: print(f'{chave} does not exist')
 
     def PutAll(self, request, context):
         print('PutAll')
@@ -158,7 +227,7 @@ class KeyValueStore(project_pb2_grpc.KeyValueStoreServicer):
         for tupla in request:
             res = self.Put(tupla,context)
             respostas.append(res)
-        print(cache_dictionary)
+        # print(cache_dictionary)
         return iter(respostas)
     
     def GetAll(self, request, context):
@@ -167,7 +236,7 @@ class KeyValueStore(project_pb2_grpc.KeyValueStoreServicer):
         for tupla in request:
             res = self.Get(tupla,context)
             respostas.append(res)
-        print(cache_dictionary)
+        # print(cache_dictionary)
         return iter(respostas)
     
     def DelAll(self, request, context):
@@ -176,78 +245,78 @@ class KeyValueStore(project_pb2_grpc.KeyValueStoreServicer):
         for tupla in request:
             res = self.Del(tupla,context)
             respostas.append(res)
-        print(cache_dictionary)
+        # print(cache_dictionary)
         return iter(respostas)
     
-    def Trim(self, request, context):
-        chave = request.key
-        if chave in cache_dictionary:
-            valores = cache_dictionary[chave]
-            versao = None
-            valor = valores[-1][0]
-            self.Del(request,context)
-            publish(client, f'UPDATE_KEY-{chave}-TO-{valor}-VALUE-IN-{versao}-VERSION')
-            resposta = project_pb2.KeyValueVersionReply(
-                    key=chave, val=valor, ver=versao
-                )
-            print(cache_dictionary)
-            return resposta
-        else: print(f'{chave} does not exist')
+    # def Trim(self, request, context):
+    #     chave = request.key
+    #     if chave in cache_dictionary:
+    #         valores = cache_dictionary[chave]
+    #         versao = None
+    #         valor = valores[-1][0]
+    #         self.Del(request,context)
+    #         publish(client, f'UPDATE_KEY-{chave}-TO-{valor}-VALUE-IN-{versao}-VERSION')
+    #         resposta = project_pb2.KeyValueVersionReply(
+    #                 key=chave, val=valor, ver=versao
+    #             )
+    #         print(cache_dictionary)
+    #         return resposta
+    #     else: print(f'{chave} does not exist')
     
-    def GetRange(self, request, context):
-        print('GetRange')
+    # def GetRange(self, request, context):
+    #     print('GetRange')
 
-        chavefr = request.fr.key
-        chaveto = request.to.key
-        versaofr = request.fr.ver
-        versaoto = request.to.ver
+    #     chavefr = request.fr.key
+    #     chaveto = request.to.key
+    #     versaofr = request.fr.ver
+    #     versaoto = request.to.ver
 
-        if versaofr >= versaoto: versao = versaofr
-        else: versao = versaoto
+    #     if versaofr >= versaoto: versao = versaofr
+    #     else: versao = versaoto
 
-        chaves_ordenadas = sorted(cache_dictionary.keys())
-        print(chaves_ordenadas)
+    #     chaves_ordenadas = sorted(cache_dictionary.keys())
+    #     print(chaves_ordenadas)
 
-        respostas = []
+    #     respostas = []
 
-        for chave in chaves_ordenadas:
-            if chave >= chavefr and chave <= chaveto:
-                valores = cache_dictionary[chave]
-                if not versao: 
-                    resposta = project_pb2.KeyValueVersionReply(
-                         key=chave, val=valores[-1][0], ver=valores[-1][1]
-                        )
-                    respostas.append(resposta)
-                else: 
-                    for valor, valor_versao in reversed(valores):
-                        if valor_versao <= versao:
-                            resposta = project_pb2.KeyValueVersionReply(
-                            key=chave, val=valor, ver=valor_versao
-                            )
-                            respostas.append(resposta)
+    #     for chave in chaves_ordenadas:
+    #         if chave >= chavefr and chave <= chaveto:
+    #             valores = cache_dictionary[chave]
+    #             if not versao: 
+    #                 resposta = project_pb2.KeyValueVersionReply(
+    #                      key=chave, val=valores[-1][0], ver=valores[-1][1]
+    #                     )
+    #                 respostas.append(resposta)
+    #             else: 
+    #                 for valor, valor_versao in reversed(valores):
+    #                     if valor_versao <= versao:
+    #                         resposta = project_pb2.KeyValueVersionReply(
+    #                         key=chave, val=valor, ver=valor_versao
+    #                         )
+    #                         respostas.append(resposta)
         
-        return iter(respostas)
+    #     return iter(respostas)
     
-    def DelRange(self, request, context):
-        print('DelRange')
+    # def DelRange(self, request, context):
+    #     print('DelRange')
 
-        chavefr = request.fr.key
-        chaveto = request.to.key
+    #     chavefr = request.fr.key
+    #     chaveto = request.to.key
 
-        chaves_ordenadas = sorted(cache_dictionary.keys())
-        print(chaves_ordenadas)
+    #     chaves_ordenadas = sorted(cache_dictionary.keys())
+    #     print(chaves_ordenadas)
 
-        respostas = []
+    #     respostas = []
 
-        for chave in chaves_ordenadas:
-            if chave >= chavefr and chave <= chaveto:
-                mensagem = project_pb2.KeyRequest(key=chave)
-                resposta = self.Del(mensagem,context)
-                respostas.append(resposta)
+    #     for chave in chaves_ordenadas:
+    #         if chave >= chavefr and chave <= chaveto:
+    #             mensagem = project_pb2.KeyRequest(key=chave)
+    #             resposta = self.Del(mensagem,context)
+    #             respostas.append(resposta)
 
-        print(cache_dictionary)
+    #     print(cache_dictionary)
         
-        return iter(respostas)
+    #     return iter(respostas)
 
 def serve(port_):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -260,13 +329,12 @@ def serve(port_):
     
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    # logging.basicConfig()
 
     port_ = '50051' if len(sys.argv) <= 1 else sys.argv[1]
 
     server = serve(port_)
-    client = connect_mqtt()
-    client.loop_start()
-    subscribe(client)
-
+    # client = connect_mqtt()
+    # client.loop_start()
+    # subscribe(client)
     server.wait_for_termination()
